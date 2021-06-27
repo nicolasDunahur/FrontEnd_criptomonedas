b87809b867a8b54cfef035e7d04ebb7cb2c927bb
nicolasDunahur <profesionalnicolasdiaz@gmail.com>
2021-06-27 02:04:58 -0300

    Consulta de api

--- src/App.js
+++ src/App.js
@@ -1,24 +1,66 @@
-import logo from './logo.svg';
+import { Fragment, useState } from 'react';
+import { Container, Button, Row, Col } from 'react-bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
+import Consulta from './components/Consulta';
 
 function App() {
+
+const [consulta, setConsulta] = useState({});
+
+  const consultarAPI = async () => {
+        try{
+        const api = await fetch('https://api.n.exchange/en/api/v1/get_price/BTCUSD/');
+        // https://nexchange2.docs.apiary.io/#reference/0/get-price/get-price?console=1
+        const consulta = await api.json();
+        setConsulta(consulta);
+        } catch (error) {
+          console.log(error);
+        }
+};
+
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <Fragment>
+    <Container>
+    <Row>
+        <Col>
+          <h1 className= "titulo">
+            Seleccione la criptomoneda que desea saber su estrategia
+          </h1>
+        </Col>
+      </Row>
+      
+    <div
+    style={
+      {
+        paddingTop:"100px",
+        display:"flex",
+        flexDirection:"column",
+        alignItems:"center"
+      }
+    }>
+    <Row>
+      <Col>
+      <Button 
+          variant="light"
+          onClick={consultarAPI}
         >
-          Learn React
-        </a>
-      </header>
+        Dolar por bitcoin
+      </Button>
+      </Col>
+      </Row>
+      <Row>
+        <Col>
+        <Consulta
+        consulta = {consulta}
+        />
+        </Col>
+      </Row>
     </div>
+    </Container>
+    </Fragment>
   );
 }
 
---
+++ src/components/Consulta.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+const Consulta = ({ consulta }) => {
+    return ( 
+        <div style= {
+            {
+                margin: "1rem",
+                padding: "3rem",
+                borderRadius: ".9rem",
+                backgroundColor: "#ffffff",
+                maxWidth: "800px",
+                color: "black"
+            }
+        }>
+            
+            <p> - ${consulta.price} - </p>
+        </div>
+     );
+}
+ 
+export default Consulta;
